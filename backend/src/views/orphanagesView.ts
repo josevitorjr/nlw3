eba28336427ea9c7fde8dd813897eb40dba7283d
josevitorjr <josevitorrjunior@outlook.com>
2020-10-14 01:15:29 -0300

    add orphanages view

--- backend/src/views/orphanagesView.ts
+++ backend/src/views/orphanagesView.ts
@@ -1,4 +1,6 @@
 import Orphanage from '../models/Orphanage';
+import ImagesView from './imagesView';
+
 export default {
     render(orphanage: Orphanage){
         return {
@@ -10,6 +12,10 @@ export default {
             instructions: orphanage.instructions,
             opening_hours: orphanage.opening_hours,
             open_on_weekends: orphanage.open_on_weekends,
+            images: ImagesView.renderMany(orphanage.images)
         };
+    },
+    renderMany(orphanages: Orphanage[]){
+        return orphanages.map(orphanage => this.render(orphanage));
     }
 }
