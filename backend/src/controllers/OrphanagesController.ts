b09bc87b5b897fbc3f07989010f23dd82cf3d029
josevitorjr <josevitorrjunior@outlook.com>
2020-10-14 01:04:18 -0300

    add Orphanges Models, Controllers

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from '../models/Orphanage';
+
+export default {
+
+    async index(request: Request, response: Response){
+        const orphanageRepository = getRepository(Orphanage);
+
+        const orphanages = await orphanageRepository.find({
+            relations: ['images']
+        });
+
+        return response.json(orphanages);
+    },
+
+    async show(request: Request, response: Response){
+        const { id } = request.params;
+        const orphanageRepository = getRepository(Orphanage);
+
+        const orphanage = await orphanageRepository.findOneOrFail(id,{
+            relations: ['images']
+        });
+
+        return response.json(orphanage);
+    },
+
+    async create(request: Request, response: Response){
+        const {
+            name, 
+            latitude, 
+            longitude, 
+            about, 
+            instructions, 
+            opening_hours, 
+            open_on_weekends
+        } = request.body;
+    
+        const orphanagesRepository = getRepository(Orphanage);
+    
+        const requestImages = request.files as Express.Multer.File[];
+        const images = requestImages.map(image => {
+            return { path: image.filename}
+        })
+
+        const orphanage = orphanagesRepository.create({
+            name, 
+            latitude, 
+            longitude, 
+            about, 
+            instructions, 
+            opening_hours, 
+            open_on_weekends,
+            images
+        });
+    
+        await orphanagesRepository.save(orphanage);
+    
+        return response.status(201).json(orphanage);
+    }
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
+
 import './database/connection';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/users', (req, res) => {
-    return res.json({ message: 'Hello Word' });
-});
+app.use(routes);
 
 app.listen(3333);
