082649437f12e37a2fb435e0e70407a4cdfb0a3e
josevitorjr <josevitorrjunior@outlook.com>
2020-10-15 19:45:17 -0300

    consume detail api

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import { Map, TileLayer , Marker, Popup} from 'react-leaflet';
-import Leaflet from 'leaflet';
 
 import mapMarkerImg from '../images/map-marker.svg';
 import mapIcon from "../utils/mapIcon";
 
 import '../styles/pages/orphanages-map.css';
 import 'leaflet/dist/leaflet.css';
+import api from '../services/api';
 
+interface Orphanage {
+    id: number;
+    latitude: number;
+    longitude: number;
+    name: string;
+}
 
 function OrphanagesMap() {
+    const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+    useEffect(() => {
+        api.get('orphanages').then(response => {
+            setOrphanages(response.data);
+        })
+    }, []);
+
     return (
         <div id="page-map">
             <aside>
@@ -28,23 +42,24 @@ function OrphanagesMap() {
                 </footer>
             </aside>
 
-            <Map 
-                center={[-22.4790114,-44.4824739]}
-                zoom={13.5}
-                style={{width:'100%', height: '100%'}}
-            >
-                <TileLayer 
-                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-                />
+            <Map center={[-22.4790114,-44.4824739]} zoom={15} style={{width:'100%', height: '100%'}}>
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
 
-                <Marker icon = {mapIcon} position={[-22.4790114,-44.4824739]}>
-                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
-                        Lar das Crianças
-                        <Link to="/orphanages/1">
-                            <FiArrowRight size={20} color="#FFF"/>
-                        </Link>
-                    </Popup>
-                </Marker>
+                {
+                    orphanages.map(orphanage => {
+                        return(
+                            <Marker key={orphanage.id} icon={mapIcon} position={[orphanage.latitude,orphanage.longitude]}>
+                                <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                                    {orphanage.name}
+                                    <Link to={`/orphanages/${orphanage.id}`}>
+                                        <FiArrowRight size={20} color="#FFF"/>
+                                    </Link>
+                                </Popup>
+                            </Marker>
+                        )
+                    })
+                }
+                
             </Map>
 
             <Link to="/orphanages/create" className="create-orphanage">
--- web/src/utils/mapIcon.ts
+++ web/src/utils/mapIcon.ts
@@ -5,9 +5,9 @@ import mapMarkerImg from '../images/map-marker.svg';
 const mapIcon = Leaflet.icon({
     iconUrl: mapMarkerImg,
   
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [0, -60]
+    iconSize: [48, 60],
+    iconAnchor: [24, 60],
+    popupAnchor: [168, 10]
   })
 
   export default mapIcon;
